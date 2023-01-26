cc467c252ef471da0ee5e0fdf2501c1e65ffc5a6
Yu <earlc.yu@gmail.com>
2023-01-26 11:25:53 +0800

    added env variables

--- config.py
+++
@@ -1,9 +0,0 @@
-api_key = 'AIzaSyA_BQkX-_rscKye3gIgRWANtzEpgZC2ur8'
-
-channels = {
-    'BicycleDutch': 'UC67YlPrRvsO117gFDM7UePg',
-    'CityNerd': 'UCfgtNfWCtsLKutY-BHzIb9Q',
-    'City Beautiful': 'UCGc8ZVCsrR3dAuhvUbkbToQ',
-    'Not Just Bikes': 'UC0intLFzLaudFG-xAvUEO-A',
-    'RMTransit': 'UCf4iKXL_SJQ5d0qsKkboRRQ'
-}
